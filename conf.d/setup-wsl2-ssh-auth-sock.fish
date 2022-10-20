a7768329b4504ad79cfa609e4c5d5ceb0dd8a606
Habu <habu1010+github@gmail.com>
2022-10-20 13:41:03 +0900

    add: Setup SSH auth socket for WSL2
    Forward SSH authentication sockets on WSL2 to
    ssh-agent on Windows using socat and npiperelay.

---
+++ conf.d/setup-wsl2-ssh-auth-sock.fish
@@ -0,0 +1,30 @@
+﻿if ! string length -q $WSL_DISTRO_NAME
+    exit 0
+end
+
+if ! type -q socat
+    echo "socat is not installed."
+    exit 0
+end
+
+set -l NPIPERELAY /mnt/c/npiperelay/npiperelay.exe
+set -l NPIPERELAY_DOWNLOAD_URL https://github.com/jstarks/npiperelay/releases/download/v0.1.0
+set -l NPIPERELAY_ZIP npiperelay_windows_amd64.zip
+
+if ! test -f $NPIPERELAY
+    if ! type -q unzip
+        echo "unzip is not installed."
+        exit 0
+    end
+    set TEMPDIR (mktemp -d)
+    curl -s -o $TEMPDIR/$NPIPERELAY_ZIP -L $NPIPERELAY_DOWNLOAD_URL/$NPIPERELAY_ZIP
+    mkdir -p (dirname $NPIPERELAY)
+    unzip -q $TEMPDIR/$NPIPERELAY_ZIP -d (dirname $NPIPERELAY)
+    rm -rf $TEMPDIR
+end
+
+set -x SSH_AUTH_SOCK $HOME/.ssh/agent.sock
+if ! ss -a | grep -q $SSH_AUTH_SOCK
+    rm -f $SSH_AUTH_SOCK
+    setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$NPIPERELAY -ei -s //./pipe/openssh-ssh-agent",nofork >/dev/null 2>&1 &
+end
